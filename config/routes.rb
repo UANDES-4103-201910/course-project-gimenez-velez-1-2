793101bcd94560f03ff664d9359d88011c7dc71a
Fede <fjgimenez@miuandes.cl>
2019-04-12 15:08:58 -0400

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root to: 'static_pages#home'
+  resources :dis_liked_posts
+  resources :liked_posts
+  resources :reports
+  resources :comments
+  resources :posts
+  resources :geofences
+  resources :user_types
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
