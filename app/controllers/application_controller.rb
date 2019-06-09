aa128e70f7f12419ea9934c624f295333ff72b53
Emilio <eivelez@miuandes.cl>
2019-06-09 02:18:30 -0400

    merge fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     ## To permit attributes while registration i.e. sign up (app/views/devise/registrations/new.html.erb)
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :phone, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :phone, :address,:profile,:image])
 
     ## To permit attributes while editing a registration (app/views/devise/registrations/edit.html.erb)
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname, :phone, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :lastname, :phone, :address,:profile,:image])
   end
 end
